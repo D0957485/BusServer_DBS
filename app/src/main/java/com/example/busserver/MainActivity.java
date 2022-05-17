200c2ad6a9e9223b19315e48949adedf584dab71
D0957485 <D0957485@o365.fcu.edu.tw>
2022-05-17 16:18:43 +0800

    0517_add_hi

--- app/src/main/java/com/example/busserver/MainActivity.java
+++ app/src/main/java/com/example/busserver/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
   private AppBarConfiguration appBarConfiguration;
   private ActivityMainBinding binding;
 
-
+  //hiiiiii
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
