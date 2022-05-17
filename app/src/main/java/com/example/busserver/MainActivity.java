0be9b5e42472b7b095b0862eb68688515b5db2a6
D0957485 <D0957485@o365.fcu.edu.tw>
2022-05-17 23:21:36 +0800

    0517_test_K_hi

--- app/src/main/java/com/example/busserver/MainActivity.java
+++ app/src/main/java/com/example/busserver/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
   private ActivityMainBinding binding;
 
   //test hi
+  //test Kevin hiiiiiiiii
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
