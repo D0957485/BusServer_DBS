70c6b5c1234b62b9f4c99019747ef6027f77994c
ShenLian <alan8jo638@gmail.com>
2022-05-18 23:59:38 +0800

    final test

--- app/src/main/java/com/example/busserver/MainActivity.java
+++ app/src/main/java/com/example/busserver/MainActivity.java
@@ -20,11 +20,6 @@ public class MainActivity extends AppCompatActivity {
   private AppBarConfiguration appBarConfiguration;
   private ActivityMainBinding binding;
 
-  //test hiiiiiiiii
-  //test Lian // 518 branch test
-  //test serty8
-  //test aaabbb
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
