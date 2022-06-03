4a6824d614a353f65eafa0f80e8b961f642934b6
D0957485 <D0957485@o365.fcu.edu.tw>
2022-06-03 16:53:45 +0800

    0603_Kevin_main and startcar

--- app/src/main/java/com/example/busserver/MainActivity.java
+++ app/src/main/java/com/example/busserver/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.busserver;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -19,15 +20,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-  private TextView textView;
-  private Button button2;
-
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-  }
+    Button button = (Button) findViewById(R.id.buttonDriverLogin);
+    button.setOnClickListener(new View.OnClickListener() {
 
+      @Override
+      public void onClick (View view) {
+        Intent intent = new Intent(MainActivity.this, StartCarActivity.class);
+        startActivity(intent);
+      }
+
+    });
+
+  }
 }
+
---
+++ app/src/main/java/com/example/busserver/StartCarActivity.java
@@ -0,0 +1,24 @@
+package com.example.busserver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartCarActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_start_car);
+
+    Button buttonB = findViewById(R.id.backStart);
+    buttonB.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        finish();
+      }
+    });
+
+  }
+}
