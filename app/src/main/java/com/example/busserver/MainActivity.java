c55248e6f5d6dc0716ad43393b8347e8708e1fb0
ShenLian <alan8jo638@gmail.com>
2022-06-04 17:16:01 +0800

    for pull

--- app/src/main/java/com/example/busserver/MainActivity.java
+++ app/src/main/java/com/example/busserver/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
       @Override
       public void onClick (View view) {
-        Intent intent = new Intent(MainActivity.this, BusYa.class);
+        Intent intent = new Intent(MainActivity.this, DriverList.class);
         startActivity(intent);
       }
 
