2fc4acac37a5d849f27c347a4b8559682cc6681c
D0957485 <D0957485@o365.fcu.edu.tw>
2022-06-04 16:41:54 +0800

    0604_Kevin_page to activity

--- app/src/main/java/com/example/busserver/Company.java
+++ app/src/main/java/com/example/busserver/Company.java
@@ -3,11 +3,11 @@ package com.example.busserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class activity_company_login extends AppCompatActivity {
+public class Company extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_company_login);
+        setContentView(R.layout.activity_company);
     }
 }
--- app/src/main/java/com/example/busserver/CompanyLogin.java
+++ app/src/main/java/com/example/busserver/CompanyLogin.java
@@ -3,11 +3,11 @@ package com.example.busserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class activity_company extends AppCompatActivity {
+public class CompanyLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_company);
+        setContentView(R.layout.activity_company_login);
     }
 }
