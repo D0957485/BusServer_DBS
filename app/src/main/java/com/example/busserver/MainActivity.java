ddacb820ae2129d65afbb55fc08d418bf7db77f5
D0957485 <D0957485@o365.fcu.edu.tw>
2022-06-06 00:10:26 +0800

    0605_Kevin_DBS connect

--- app/src/main/java/com/example/busserver/MainActivity.java
+++ app/src/main/java/com/example/busserver/MainActivity.java
@@ -107,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
 
       try {
         URL url = new URL("http://10.0.2.2/GetData.php");
+//        URL url = new URL("http://192.168.0.2/GetData.php");
         // 開始宣告 HTTP 連線需要的物件，這邊通常都是一綑的
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         // 建立 Google 比較挺的 HttpURLConnection 物件
