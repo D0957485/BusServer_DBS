5a51a989b58bedeaf6989361687c0ac190e9f5d6
D0957485 <D0957485@o365.fcu.edu.tw>
2022-06-05 13:40:51 +0800

    0604_Kevin_page finish

--- app/src/main/java/com/example/busserver/MainActivity.java
+++ app/src/main/java/com/example/busserver/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.busserver;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import android.webkit.CookieManager;
+import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+  Context context = this;
+  EditText et1, et2, et3, et4;
+  WebView webView;
+  String url = "http://localhost/phpmyadmin/index.php?route=/sql&pos=0&db=bussystem&table=drive";
+  CookieManager cookieManager;
+  String cookieStr, idText;
+  ListView listView1;
 
   //0603
   @Override
